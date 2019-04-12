060fbc83be10ab375307d8d384c763b0f6d9d83a
Jak Tiano <jtiano@magicleap.com>
2019-04-12 08:40:35 -0400

    Initial Commit

---
+++ okr-generator-mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  okr-generator-mac
+//
+//  Created by Jak Tiano on 4/12/19.
+//  Copyright © 2019 Jak Tiano. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ okr-generator-mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  okr-generator-mac
+//
+//  Created by Jak Tiano on 4/12/19.
+//  Copyright © 2019 Jak Tiano. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
